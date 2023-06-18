c68c121858153d76c6f2b469a0a27f2ca71cdb62
Ofek Israel <ofekisrael95@gmail.com>
2023-06-18 19:46:43 +0300

    need to do data preprocessing

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,11 +1,12 @@
+import keras
 from keras.models import load_model
 
+def custom_accuracy(y_true, y_pred):
+    return 0.5;
 
 class ModelLoader:
-    def __init__(self):
-        self.model_file = open("app/assets/model/musickli_model.h5", "r")
-
     def predict_file(self, audio_file):
-        model = load_model(self.model_file)
+        keras.utils.get_custom_objects().update({'custom_accuracy': custom_accuracy})
+        model = load_model("./assets/model/musickli_model.h5")
         result = model.predict(audio_file)
         return result
--- app/routers/files.py
+++ app/routers/files.py
@@ -8,9 +8,7 @@ router = APIRouter(
 
 @router.post("/loadfile/")
 async def create_upload_file(audio_file: UploadFile):
-    if audio_file.size > 100 or not audio_file.filename.endswith(".wav"):
-        print(audio_file.size)
-        print(audio_file.filename)
+    if not audio_file.filename.endswith(".wav"):
         raise HTTPException(status_code=500, detail="Musickli doesn't support")
     musickli_model = ModelLoader()
     result = musickli_model.predict_file(audio_file)
