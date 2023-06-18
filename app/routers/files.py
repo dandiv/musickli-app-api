9964d8046c00df2abf5458fab9c21673090a7dfa
Ofek Israel <ofekisrael95@gmail.com>
2023-06-18 18:55:25 +0300

    wip

--- app/routers/files.py
+++ app/routers/files.py
@@ -1,17 +1,16 @@
-from fastapi import APIRouter, FastAPI, UploadFile, HTTPException
-
-from app.model_loader import ModelLoader
+from fastapi import APIRouter, UploadFile, HTTPException
+from model_loader import ModelLoader
 
 router = APIRouter(
     prefix="/files",
     tags=["files"],
 )
-app = FastAPI()
 
-
-@app.post("/loadfile/")
+@router.post("/loadfile/")
 async def create_upload_file(audio_file: UploadFile):
     if audio_file.size > 100 or not audio_file.filename.endswith(".wav"):
+        print(audio_file.size)
+        print(audio_file.filename)
         raise HTTPException(status_code=500, detail="Musickli doesn't support")
     musickli_model = ModelLoader()
     result = musickli_model.predict_file(audio_file)
